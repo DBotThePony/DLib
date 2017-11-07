a3799ae52a73f0228ca14c3df4485e05511d0b34
DBotThePony <dbotthepony@yandex.ru>
2017-11-07 20:44:20 +0700

    Finally killed nutscript

--- lua_src/dlib/modules/workarounds/nutscript.lua
+++ lua_src/dlib/modules/workarounds/nutscript.lua
@@ -14,17 +14,22 @@
 -- limitations under the License.
 
 local HOOKS_CACHE
-local SCHEMA = {}
+local patched = {}
+local patching = false
 
 local function patchHook(event)
+	if patching then return end
+	if patched[event] then return end
+	patched[event] = true
+
+	patching = true
+	HOOKS_CACHE[event] = HOOKS_CACHE[event] or {}
+	patching = false
+
 	local hookTab = HOOKS_CACHE[event]
-
-	if not hookTab then
-		DLib.Message(debug.traceback('Nutscript event ' .. event .. ' is empty/missing!'))
-		return
-	end
-
 	DLib.Message('Patched Nutscript ' .. event .. ' event')
+
+	local SCHEMA = _G.SCHEMA
 
 	return hook.Add(event, 'nutscript', function(...)
 		for funcID, func in pairs(hookTab) do
@@ -43,14 +48,12 @@ local function patchHook(event)
 	end, -10) -- as i understand nutscript wants to run in the front of all
 end
 
-local function patch()
+function DLib.patchNutscript()
 	timer.Remove('DLib.PatchNutscript')
 
 	if not nut then return end
 	if not nut.plugin then return end
 	if not nut.plugin.load then return end
-
-	SCHEMA = _G.SCHEMA or {}
 
 	local upvalName, upvalValue = debug.getupvalue(nut.plugin.load, 1)
 
@@ -66,9 +69,17 @@ local function patch()
 		return
 	end
 
+	_G.SCHEMA = _G.SCHEMA or {}
 	HOOKS_CACHE = upvalValue
 
 	setmetatable(HOOKS_CACHE, {
+		__newindex = function(self, key, value)
+			rawset(self, key, value)
+			patchHook(key)
+		end
+	})
+
+	setmetatable(_G.SCHEMA, {
 		__newindex = function(self, key, value)
 			rawset(self, key, value)
 			patchHook(key)
@@ -79,16 +90,20 @@ local function patch()
 		patchHook(event)
 	end
 
+	for event, eventData in pairs(_G.SCHEMA) do
+		patchHook(event)
+	end
+
 	DLib.Message('------------------------------')
 	DLib.Message('Nutscript successfully patched!')
 	DLib.Message('------------------------------')
 end
 
-timer.Create('DLib.PatchNutscript', 0, 1, patch)
+timer.Create('DLib.PatchNutscript', 0, 1, DLib.patchNutscript)
 
 hook.Add('DLibHookChange', 'NutscriptPatch', function(key)
 	if key == 'NutCall' then
-		patch()
+		DLib.patchNutscript()
 		hook.Remove('DLibHookChange', 'NutscriptPatch')
 	end
 end)
