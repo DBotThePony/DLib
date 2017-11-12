0b155c9e101d784df12d7122c3239c8a28c9540d
DBotThePony <dbotthepony@yandex.ru>
2017-11-12 11:51:03 +0700

    Add lastnick tracking

---
+++ lua_src/dlib/modules/client/lastnick.lua
@@ -0,0 +1,59 @@
+
+-- Copyright (C) 2017 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local LocalPlayer = LocalPlayer
+local sql = sql
+local DLib = DLib
+
+sql.Query([[
+	CREATE TABLE IF NOT EXISTS dlib_lastnick (
+		steamid VARCHAR(31) NOT NULL,
+		lastnick VARCHAR(255) NOT NULL,
+		PRIMARY KEY (steamid)
+	)
+]])
+
+function DLib.LastNick(steamid)
+	local ply = player.GetBySteamID(steamid)
+
+	if ply then
+		return ply:Nick()
+	end
+
+	local data = sql.Query('SELECT lastnick FROM dlib_lastnick WHERE steamid = ' .. SQLStr(steamid))
+
+	if not data then return false end
+	return data[1].lastnick
+end
+
+function DLib.UpdateLastNicks()
+	sql.Query('BEGIN')
+
+	for i, ply in ipairs(player.GetHumans()) do
+		local steamid, nick = SQLStr(ply:SteamID()), SQLStr(ply:Nick())
+
+		if not ply.__dlib_nickinsert then
+			sql.Query('INSERT INTO dlib_lastnick (steamid, lastnick) VALUES (' .. steamid .. ', ' .. nick .. ')')
+			ply.__dlib_nickinsert = true
+		end
+
+		sql.Query('UPDATE dlib_lastnick WHERE')
+	end
+
+	sql.Query('COMMIT')
+end
+
+timer.Create('DLib.UpdateLastNicks', 5, 0, DLib.UpdateLastNicks)
+DLib.UpdateLastNicks()
