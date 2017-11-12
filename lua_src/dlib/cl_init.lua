ee32f81cb858b252fb1e993e41abd4b3c0d29cd6
DBotThePony <dbotthepony@yandex.ru>
2017-11-12 12:46:28 +0700

    Fixed loading mistake

--- lua_src/dlib/cl_init.lua
+++ lua_src/dlib/cl_init.lua
@@ -19,9 +19,9 @@ DLib.Loader.finish(false)
 
 DLib.register('util/client/chat.lua')
 
-if not VLL_CURR_FILE then
-	DLib.Loader.loadPureCSTop('dlib/modules/client')
+DLib.Loader.loadPureCSTop('dlib/modules/client')
 
+if not VLL_CURR_FILE then
 	DLib.Loader.loadPureSHTop('dlib/autorun')
 	DLib.Loader.loadPureCSTop('dlib/autorun/client')
 end
