efe8031b34909368998b097c768d8bdce4995758
DBotThePony <dbotthepony@yandex.ru>
2017-11-20 06:54:56 +0700

    ugh

--- lua_src/dlib/modules/hook.lua
+++ lua_src/dlib/modules/hook.lua
@@ -408,8 +408,11 @@ local function chaseError(err)
 end
 
 local function xpcall(func, ...)
-	local _, Q, W, E, R, T, Y, U, I, O, P, A, S, D, F, G, H, J, K, L, Z, X, C, V, B, N, M, M = gxpcall(func, chaseError, ...)
-	return Q, W, E, R, T, Y, U, I, O, P, A, S, D, F, G, H, J, K, L, Z, X, C, V, B, N, M, M
+	local _STATUS, Q, W, E, R, T, Y, U, I, O, P, A, S, D, F, G, H, J, K, L, Z, X, C, V, B, N, M, M = gxpcall(func, chaseError, ...)
+
+	if _STATUS then
+		return Q, W, E, R, T, Y, U, I, O, P, A, S, D, F, G, H, J, K, L, Z, X, C, V, B, N, M, M
+	end
 end
 
 local function Call(event, ...)
