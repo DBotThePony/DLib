bd9e79526a0d090bd2a20b1ac922b397021b3203
DBotThePony <dbotthepony@yandex.ru>
2017-12-02 20:37:03 +0700

    Old header

--- lua_src/dlib/net/net.lua
+++ lua_src/dlib/net/net.lua
@@ -7,6 +7,12 @@
 
 --     http://www.apache.org/licenses/LICENSE-2.0
 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
 DLib.netModule = DLib.netModule or {}
 DLib.nativeNet = DLib.nativeNet or net
 
@@ -130,3 +136,4 @@ function net.CreateMessage(length, read)
 end
 
 DLib.simpleInclude('net/net_object.lua')
+DLib.simpleInclude('net/net_ext.lua')
