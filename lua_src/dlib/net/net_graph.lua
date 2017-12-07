e579dac03d7d96b0b8ed266d20c16d8b420709ed
DBotThePony <dbotthepony@yandex.ru>
2017-12-07 07:17:53 +0700

    Updated net graph, added values

--- lua_src/dlib/net/net_graph.lua
+++ lua_src/dlib/net/net_graph.lua
@@ -19,6 +19,7 @@ local net = DLib.netModule
 local net_graph = GetConVar('net_graph')
 
 local ipairs = ipairs
+local pairs = pairs
 local table = table
 local math = math
 local ScrW, ScrH = ScrW, ScrH
@@ -64,11 +65,9 @@ function net.RegisterGraphGroup(groupName, groupID, colorSeed)
 	local t = net.GraphGroups[groupID]
 	t.name = groupName
 	t.id = groupID
-	t.color = ColorFromSeed(colorSeed):SetAlpha(150)
+	t.color = ColorFromSeed(colorSeed)
 	return t
 end
-
-net.RegisterGraphGroup('other').color = Color(255, 255, 255)
 
 function net.BindMessageGroup(networkID, groupID)
 	groupID = groupID:lower()
@@ -79,12 +78,18 @@ end
 
 local abscissaColor = Color(200, 200, 200, 150)
 local textColorLevel = Color(200, 200, 200)
-local totalColor = Color(0, 0, 0, 150)
+local totalColor = Color(0, 0, 0)
 
 surface.CreateFont('DLib.NetGraphLevel', {
 	font = 'Roboto',
 	size = 16,
 	weight = 400
+})
+
+surface.CreateFont('DLib.NetGraphTypes', {
+	font = 'Roboto',
+	size = 18,
+	weight = 500
 })
 
 local function HUDPaint()
@@ -113,36 +118,69 @@ local function HUDPaint()
 	surface.DrawRect(0, abscissa, W, 5)
 
 	local prevNodes = {}
-	local hitGroups = {}
+	local hitGroups
+	local allGroups = {}
+
+	local function doDrawGraph(i, group, value)
+		if group == '__TOTAL' then group = '__total' end
+		local color, lastX, lastY
+		local prev = prevNodes[group]
+		local x, y = i * nodeStep, S - value / scale * diff
+
+		if prev then
+			lastX, lastY = prev[1], prev[2]
+		else
+			lastX, lastY = (i - 1) * nodeStep, S
+		end
+
+		prevNodes[group] = {x, y}
+		color = net.GraphGroups[group].color
+
+		surface.SetDrawColor(color)
+		surface.DrawLine(lastX, lastY, x, y)
+	end
 
 	for i, frame in ipairs(net.Graph) do
+		hitGroups = {}
+
 		for group, value in pairs(frame) do
-			local color, lastX, lastY
-			local prev = prevNodes[group]
-			local x, y = i * nodeStep, S - value / scale * diff
+			doDrawGraph(i, group, value)
+			hitGroups[group] = true
+		end
 
-			if prev then
-				lastX, lastY = prev[1], prev[2]
-			else
-				lastX, lastY = x, y
+		for group, data in pairs(prevNodes) do
+			if not hitGroups[group] then
+				data[1] = (i - 1) * nodeStep
+				data[2] = S
+				doDrawGraph(i, group, 0)
 			end
-
-			prevNodes[group] = {x, y}
-
-			if group == '__TOTAL' then
-				color = totalColor
-			else
-				color = net.GraphGroups[group].color
-			end
-
-			surface.SetDrawColor(color)
-			surface.DrawLine(lastX, lastY, x, y)
 		end
 	end
 
-	for group, _ in pairs(prevNodes) do
+	local tx, ty = 10, abscissa + 20
+	surface.SetFont('DLib.NetGraphTypes')
 
+	for group, _ in pairs(prevNodes) do
+		surface.SetTextColor(net.GraphGroups[group].color)
+		surface.SetTextPos(tx, ty)
+		local t = net.GraphGroups[group].name
+		surface.DrawText(t)
+		local w, h = surface.GetTextSize(t)
+		tx = tx + 20 + w
+
+		if tx >= W - 60 then
+			tx = 10
+			ty = ty + 20
+		end
 	end
 end
 
 hook.Add('HUDPaint', 'DLib.NetGraph', HUDPaint)
+
+-- Some known group
+net.RegisterGraphGroup('Other').color = Color(255, 255, 255)
+net.RegisterGraphGroup('Total', '__TOTAL').color = totalColor
+net.RegisterGraphGroup('Usermessages')
+
+-- some known messages
+net.BindMessageGroup('dlib.umsg', 'Usermessages')
