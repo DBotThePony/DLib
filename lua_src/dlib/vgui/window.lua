25e112900d333f578f67abc7ae1f09527a03c131
DBotThePony <dbotthepony@yandex.ru>
2017-12-28 15:34:20 +0700

    Window stretching

--- lua_src/dlib/vgui/window.lua
+++ lua_src/dlib/vgui/window.lua
@@ -13,10 +13,85 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License.
 
-local PANEL = {}
-DLib.VGUI.Window = PANEL
+local surface = surface
+local gui = gui
+local vgui = vgui
+local input = input
 
--- TODO: Stretching
+local PANEL = {}
+
+function PANEL:Init()
+	self.tapped = false
+	self.target = NULL
+	self.effects = true
+	self.tapX, self.tapY = 0, 0
+	self.oldW, self.oldH = 0, 0
+	self.minW, self.minH = 100, 30
+	self.currX, self.currY = 0, 0
+	self.translucent = 0
+	self:SetCursor('sizenwse')
+
+	hook.Add('PostRenderVGUI', self, self.PostRenderVGUI)
+end
+
+function PANEL:Paint(w, h)
+	surface.SetDrawColor(140, 140, 140, 150)
+	surface.DrawRect(0, 0, w, h)
+end
+
+function PANEL:SetTarget(targetPanel)
+	self.target = targetPanel
+	return self
+end
+
+function PANEL:PostRenderVGUI()
+	if not self.tapped and not self.fadeout then return end
+	surface.SetDrawColor(self.translucent * 2.5, self.translucent * 2.5, self.translucent * 2.5, self.translucent)
+	surface.DrawRect(self.currX, self.currY, self:GetNewDimensions())
+end
+
+function PANEL:OnMousePressed(key)
+	if key ~= MOUSE_LEFT then return end
+	self.tapped = true
+	self.tapX, self.tapY = gui.MousePos()
+	self.oldW, self.oldH = self.target:GetSize()
+	self.translucent = 0
+	self.currX, self.currY = self.target:GetPos()
+end
+
+function PANEL:Think()
+	if self.tapped then
+		if not input.IsMouseDown(MOUSE_LEFT) then
+			self:OnMouseReleased(MOUSE_LEFT)
+			return
+		end
+
+		self.translucent = Lerp(FrameTime() * 5, self.translucent, 100)
+	end
+
+	if self.fadeout then
+		self.translucent = Lerp(FrameTime() * 8, self.translucent, 0)
+		self.fadeout = self.translucent > 1
+	end
+end
+
+function PANEL:GetNewDimensions()
+	local x, y = gui.MousePos()
+	local diffX, diffY = x - self.tapX, y - self.tapY
+	return math.max(self.oldW + diffX, self.minW), math.max(self.oldH + diffY, self.minH)
+end
+
+function PANEL:OnMouseReleased()
+	self.tapped = false
+	self.fadeout = true
+	local w, h = self:GetNewDimensions()
+	self.target:SetSize(w, h)
+end
+
+vgui.Register('DLib_ResizeTap', PANEL, 'EditablePanel')
+
+PANEL = {}
+DLib.VGUI.Window = PANEL
 
 function PANEL:Init()
 	self:SetSize(ScrW() - 100, ScrH() - 100)
@@ -24,6 +99,19 @@ function PANEL:Init()
 	self:MakePopup()
 	self:SetTitle('DLib Window')
 	self:SetSkin('DLib_Black')
+
+	self.bottomBar = vgui.Create('EditablePanel', self)
+	local bar = self.bottomBar
+	bar:Dock(BOTTOM)
+	bar:SetSize(0, 16)
+	bar:SetMouseInputEnabled(true)
+
+	self.bottomTap = vgui.Create('DLib_ResizeTap', bar)
+	local tap = self.bottomTap
+	tap:Dock(RIGHT)
+	tap:SetSize(16, 16)
+	tap:SetMouseInputEnabled(true)
+	tap:SetTarget(self)
 end
 
 function PANEL:UpdateSize(w, h)
