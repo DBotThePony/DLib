7afa7e0df4a189e93c46ab6b873b43f739ee5245
DBotThePony <dbotthepony@yandex.ru>
2017-12-30 20:00:58 +0700

    Simple loading stages

---
+++ lua_src/dlib/luabridge/loading_stages.lua
@@ -0,0 +1,41 @@
+
+-- Copyright (C) 2017-2018 DBot
+
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+
+--     http://www.apache.org/licenses/LICENSE-2.0
+
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+
+local hook = hook
+local DLib = DLib
+local CurTime = CurTime
+
+local init_post_entity = CurTime() > 60
+local initialize = CurTime() > 60
+
+function _G.AreEntitiesAvaliable()
+	return init_post_entity
+end
+
+function _G.IsGamemodeAvaliable()
+	return initialize
+end
+
+if not init_post_entity then
+	hook.Add('InitPostEntity', 'DLib.LoadingStages', function()
+		init_post_entity = true
+	end)
+end
+
+if not initialize then
+	hook.Add('Initialize', 'DLib.LoadingStages', function()
+		initialize = true
+	end)
+end
