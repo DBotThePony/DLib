c0f431bd1e5b92a9b22f87077facae7dfb1cda1c
DBotThePony <dbotthepony@yandex.ru>
2017-12-30 18:56:42 +0700

    Updated linter arguments

--- luacheck.sh
+++ luacheck.sh
@@ -1,6 +1,6 @@
 
 if test -f '/usr/bin/luacheck' || test -f '/usr/local/bin/luacheck'; then
-	luacheck lua_src/* || exit 1
+	luacheck lua_src/* --no-max-string-line-length --no-max-comment-line-length --no-max-line-length || exit 1
 else
 	echo 'No luacheck found, nothing to test.'
 	exit 0
