2bb381c8c51a4bbe47c199689be8d30e93d5d927
DBotThePony <dbotthepony@yandex.ru>
2018-02-25 13:12:28 +0700

    Modified hud shifting to gun shift

--- lua_src/dlib/modules/hudcommons/position2.lua
+++ lua_src/dlib/modules/hudcommons/position2.lua
@@ -16,16 +16,21 @@
 --
 
 local ENABLE_SHIFTING = CreateConVar('dlib_hud_shift', '1', {FCVAR_ARCHIVE}, 'Enable HUD shifting')
+local ENABLE_SHIFTING_WEAPON = CreateConVar('dlib_hud_shift_wp', '1', {FCVAR_ARCHIVE}, 'Enable HUD shifting with weapons')
 local ENABLE_SHIFTING_SV = CreateConVar('sv_dlib_hud_shift', '1', {FCVAR_REPLICATED, FCVAR_NOTIFY}, 'SV Override: Enable HUD shifting')
+local ENABLE_SHIFTING_SV_WEAPON = CreateConVar('sv_dlib_hud_shift_wp', '1', {FCVAR_REPLICATED, FCVAR_NOTIFY}, 'SV Override: Enable HUD shifting with weapons')
 local HUDCommons = HUDCommons
 local table = table
+local RealTime = RealTime
 local math = math
 
 HUDCommons.Position2 = HUDCommons.Position2 or {}
 local Pos2 = HUDCommons.Position2
 
 Pos2.ShiftX = 0
+Pos2.ShiftX_Weapon = 0
 Pos2.ShiftY = 0
+Pos2.ShiftY_Weapon = 0
 Pos2.LastAngle = Angle(0, 0, 0)
 
 Pos2.XPositions = Pos2.XPositions or {}
@@ -94,11 +99,11 @@ Pos2.GetPosition = Pos2.GetPos
 local function UpdatePositions()
 	if ENABLE_SHIFTING:GetBool() and ENABLE_SHIFTING_SV:GetBool() then
 		for k, v in pairs(Pos2.XPositions) do
-			Pos2.XPositions_modified[v] = Pos2.XPositions_original[v] + Pos2.ShiftX
+			Pos2.XPositions_modified[v] = Pos2.XPositions_original[v] + Pos2.ShiftX + Pos2.ShiftX_Weapon
 		end
 
 		for k, v in pairs(Pos2.YPositions) do
-			Pos2.YPositions_modified[v] = Pos2.YPositions_original[v] + Pos2.ShiftY
+			Pos2.YPositions_modified[v] = Pos2.YPositions_original[v] + Pos2.ShiftY + Pos2.ShiftY_Weapon
 		end
 	else
 		for k, v in pairs(Pos2.XPositions) do
@@ -111,7 +116,7 @@ local function UpdatePositions()
 	end
 end
 
-local function UpdateShift()
+local function UpdateShift(delta)
 	if not ENABLE_SHIFTING:GetBool() then return end
 	if not ENABLE_SHIFTING_SV:GetBool() then return end
 
@@ -121,15 +126,15 @@ local function UpdateShift()
 	local changePitch = math.AngleDifference(ang.p, Pos2.LastAngle.p)
 	local changeYaw = math.AngleDifference(ang.y, Pos2.LastAngle.y)
 
-	Pos2.LastAngle = LerpAngle(FrameTime() * 33, Pos2.LastAngle, ang)
+	Pos2.LastAngle = LerpAngle(delta * 33, Pos2.LastAngle, ang)
 
 	Pos2.ShiftX = math.Clamp(Pos2.ShiftX + changeYaw * 1.8, -30, 30)
 	Pos2.ShiftY = math.Clamp(Pos2.ShiftY - changePitch * 1.8, -20, 20)
 
 	local oldX, oldY = Pos2.ShiftX, Pos2.ShiftY
 
-	Pos2.ShiftX = Pos2.ShiftX - Pos2.ShiftX * FrameTime() * 22
-	Pos2.ShiftY = Pos2.ShiftY - Pos2.ShiftY * FrameTime() * 22
+	Pos2.ShiftX = Pos2.ShiftX - Pos2.ShiftX * delta * 22
+	Pos2.ShiftY = Pos2.ShiftY - Pos2.ShiftY * delta * 22
 
 	if oldX > 0 and Pos2.ShiftX < 0 or oldX < 0 and Pos2.ShiftX > 0 then
 		Pos2.ShiftX = 0
@@ -140,8 +145,90 @@ local function UpdateShift()
 	end
 end
 
+local lastWeaponPosX = 0
+local lastWeaponPosY = 0
+local lastWeaponPosZ = 0
+local lastWeapon = NULL
+local WorldToLocal = WorldToLocal
+local IsValid = FindMetaTable('Entity').IsValid
+
+local function UpdateWeaponShift(delta)
+	if not ENABLE_SHIFTING:GetBool() then return end
+	if not ENABLE_SHIFTING_WEAPON:GetBool() then return end
+	if not ENABLE_SHIFTING_SV:GetBool() then return end
+	if not ENABLE_SHIFTING_SV_WEAPON:GetBool() then return end
+
+	Pos2.ShiftX_Weapon = Pos2.ShiftX_Weapon - Pos2.ShiftX_Weapon * delta * 22
+	Pos2.ShiftY_Weapon = Pos2.ShiftY_Weapon - Pos2.ShiftY_Weapon * delta * 22
+
+	local ply = HUDCommons.SelectPlayer()
+	local wep = ply:GetActiveWeapon()
+	if not IsValid(wep) then return end
+	local plyPos = ply:EyePos()
+	local plyAng = ply:EyeAngles()
+	local xs, ys, zs = 0, 0, 0
+	local amount = 0
+
+	if ply.GetViewModel then
+		local view = ply:GetViewModel()
+
+		if IsValid(view) then
+			local bones = view:GetBoneCount()
+			amount = amount + bones
+
+			for bone = 0, bones - 1 do
+				local bpos, bang = view:GetBonePosition(bone)
+				local npos, nang = WorldToLocal(bpos, bang, plyPos, plyAng)
+				xs = xs + npos.x
+				ys = ys + npos.y
+				zs = zs + npos.z
+			end
+		end
+	end
+
+	if ply.GetHands then
+		local hands = ply:GetHands()
+
+		if IsValid(hands) then
+			local bones = hands:GetBoneCount()
+			amount = amount + bones
+
+			for bone = 0, bones - 1 do
+				local bpos, bang = hands:GetBonePosition(bone)
+				local npos, nang = WorldToLocal(bpos, bang, plyPos, plyAng)
+				xs = xs + npos.x
+				ys = ys + npos.y
+				zs = zs + npos.z
+			end
+		end
+	end
+
+	if amount == 0 then return end
+
+	xs, ys, zs = xs / amount, ys / amount, zs / amount
+	local changeX = xs - lastWeaponPosX
+	local changeY = ys - lastWeaponPosY
+	local changeZ = zs - lastWeaponPosZ
+
+	--print(changeX, changeY, changeZ)
+
+	lastWeaponPosX = xs
+	lastWeaponPosY = ys
+	lastWeaponPosZ = zs
+
+	Pos2.ShiftX_Weapon = math.Clamp(Pos2.ShiftX_Weapon + (changeX / delta) - (changeY / delta), -10, 10)
+	Pos2.ShiftY_Weapon = math.Clamp(Pos2.ShiftY_Weapon + (changeZ / delta), -10, 10)
+end
+
+local lastThink = RealTime()
+
 local function Think()
-	UpdateShift()
+	local time = RealTime()
+	local delta = time - lastThink
+	lastThink = time
+	if delta == 0 then return end
+	UpdateShift(delta)
+	UpdateWeaponShift(delta)
 	UpdatePositions()
 end
 
