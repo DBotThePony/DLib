1f7648f3aee1898376a3708766a7d85bb45ed264
DBotThePony <dbotthepony@yandex.ru>
2018-03-26 17:24:13 +0700

    Luaify scrw scrh

--- lua_src/dlib/luabridge/luaify.lua
+++ lua_src/dlib/luabridge/luaify.lua
@@ -25,9 +25,22 @@ local RealTimeC = RealTimeC
 _G.CurTimeC = CurTimeC or CurTime
 local CurTimeC = CurTimeC
 
+_G.ScrWC = ScrWC or ScrW
+_G.ScrHC = ScrHC or ScrH
+render.SetViewPortC = render.SetViewPortC or render.SetViewPort
+
+local ScrWC = ScrWC
+local ScrHC = ScrHC
+local render = render
+local type = type
+local assert = assert
+
 DLib.luaify_rTime = RealTimeC()
 DLib.luaify_cTime = CurTimeC()
 DLib.luaify_frameNum = FrameNumberC()
+
+DLib.luaify_scrw = ScrWC()
+DLib.luaify_scrh = ScrHC()
 
 function _G.RealTime()
 	return DLib.luaify_rTime
@@ -41,8 +54,29 @@ function _G.CurTime()
 	return DLib.luaify_cTime
 end
 
+function _G.ScrW()
+	return DLib.luaify_scrw
+end
+
+function _G.ScrH()
+	return DLib.luaify_scrh
+end
+
+function _G.render.SetViewPort(x, y, w, h)
+	assert(type(w) == 'number' and w > 0, 'invalid W value')
+	assert(type(h) == 'number' and h > 0, 'invalid H value')
+	assert(type(x) == 'number' and x > 0, 'invalid X value')
+	assert(type(y) == 'number' and y > 0, 'invalid Y value')
+	DLib.luaify_scrw = w
+	DLib.luaify_scrh = h
+	return render.SetViewPortC(x, y, w, h)
+end
+
 hook.Add('PreRender', 'DLib.UpdateFrameOptions', function()
 	DLib.luaify_rTime = RealTimeC()
 	DLib.luaify_cTime = CurTimeC()
 	DLib.luaify_frameNum = FrameNumberC()
+
+	DLib.luaify_scrw = ScrWC()
+	DLib.luaify_scrh = ScrHC()
 end, -9)
