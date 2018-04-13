d50c560b850993f4c54c694a4249ced40b37b984
DBotThePony <dbotthepony@yandex.ru>
2018-04-13 17:18:59 +0700

    Added moar

--- lua_src/dlib/modules/i18n/cl_i18n.lua
+++ lua_src/dlib/modules/i18n/cl_i18n.lua
@@ -17,27 +17,84 @@ local i18n = i18n
 local hook = hook
 local unpack = unpack
 
-local function languageWatchdog(self)
-	self:_SetTextDLib(i18n.localize(self._DLibLocalize, unpack(self._DLibLocalizeArgs)))
-end
-
-local function SetText(self, text, ...)
-	if not i18n.exists(text) then
-		hook.Remove('DLib.LanguageChanged2', self)
-		return self:_SetTextDLib(text, ...)
+do
+	local function languageWatchdog(self)
+		self:_SetTextDLib(i18n.localize(self._DLibLocalize, unpack(self._DLibLocalizeArgs)))
 	end
 
-	hook.Add('DLib.LanguageChanged2', self, languageWatchdog)
-	self._DLibLocalize = text
-	self._DLibLocalizeArgs = {...}
-	return self:_SetTextDLib(i18n.localize(text, ...))
+	local function SetText(self, text, ...)
+		if not i18n.exists(text) then
+			hook.Remove('DLib.LanguageChanged2', self)
+			return self:_SetTextDLib(text, ...)
+		end
+
+		hook.Add('DLib.LanguageChanged2', self, languageWatchdog)
+		self._DLibLocalize = text
+		self._DLibLocalizeArgs = {...}
+		return self:_SetTextDLib(i18n.localize(text, ...))
+	end
+
+	local function vguiPanelCreated(self)
+		if not self.SetText then return end
+		if self:GetClassName():lower():find('textentry') then return end
+
+		self._SetTextDLib = self._SetTextDLib or self.SetText
+		self.SetText = SetText
+	end
+
+	hook.Add('VGUIPanelCreated', 'DLib.I18n', vguiPanelCreated)
 end
 
-local function vguiPanelCreated(self)
-	if not self.SetText then return end
+do
+	local function languageWatchdog(self)
+		self:_SetLabelDLib(i18n.localize(self._DLibLocalize, unpack(self._DLibLocalizeArgs)))
+	end
 
-	self._SetTextDLib = self._SetTextDLib or self.SetText
-	self.SetText = SetText
+	local function SetLabel(self, text, ...)
+		if not i18n.exists(text) then
+			hook.Remove('DLib.LanguageChanged2', self)
+			return self:_SetLabelDLib(text, ...)
+		end
+
+		hook.Add('DLib.LanguageChanged2', self, languageWatchdog)
+		self._DLibLocalize = text
+		self._DLibLocalizeArgs = {...}
+		return self:_SetLabelDLib(i18n.localize(text, ...))
+	end
+
+	local function vguiPanelCreated(self)
+		if not self.SetLabel then return end
+
+		self._SetLabelDLib = self._SetLabelDLib or self.SetLabel
+		self.SetLabel = SetLabel
+	end
+
+	hook.Add('VGUIPanelCreated', 'DLib.I18n_Label', vguiPanelCreated)
 end
 
-hook.Add('VGUIPanelCreated', 'DLib.I18n', vguiPanelCreated)
+do
+	local function languageWatchdog(self)
+		self:_SetTitleDLib(i18n.localize(self._DLibLocalize, unpack(self._DLibLocalizeArgs)))
+	end
+
+	local function SetTitle(self, text, ...)
+		if not i18n.exists(text) then
+			hook.Remove('DLib.LanguageChanged2', self)
+			return self:_SetTitleDLib(text, ...)
+		end
+
+		hook.Add('DLib.LanguageChanged2', self, languageWatchdog)
+		self._DLibLocalize = text
+		self._DLibLocalizeArgs = {...}
+		return self:_SetTitleDLib(i18n.localize(text, ...))
+	end
+
+	local function vguiPanelCreated(self)
+		if not self.SetTitle then return end
+
+		self._SetTitleDLib = self._SetTitleDLib or self.SetTitle
+		self.SetTitle = SetTitle
+	end
+
+	hook.Add('VGUIPanelCreated', 'DLib.I18n_Title', vguiPanelCreated)
+end
