6d244752ddd9e1a030dfdaf6daf26759db6e09e6
DBotThePony <dbotthepony@yandex.ru>
2019-04-20 23:38:26 +0700

    Cubic root distance

--- lua_src/dlib/modules/hudcommons/base/crosshairs.lua
+++ lua_src/dlib/modules/hudcommons/base/crosshairs.lua
@@ -224,7 +224,7 @@ function meta:InternalDrawCrosshair(ply)
 	local wclass = self:GetVarWeaponClass()
 
 	if mapping ~= 'DrawCrosshairRPG' and mapping ~= 'DrawCrosshairNade' and self.DYNAMIC_CROSSHAIR:GetBool() and (self.DYNAMIC_CROSSHAIR_ALWAYS:GetBool() or lastShouldDrawLocalPlayer and ply == LocalPlayer()) then
-		local newAcc = (tr.StartPos:Distance(tr.HitPos) / 256):sqrt() * (90 / (lastFOV or ply:GetFOV())):pow(1.19) * 0.4
+		local newAcc = (tr.StartPos:Distance(tr.HitPos) / 256):pow(1 / 3) * (90 / (lastFOV or ply:GetFOV())):pow(1.19) * 0.4
 		self.lastDistAccuracyMult = Lerp(FrameTime() * 4, self.lastDistAccuracyMult, newAcc)
 		accuracy = self.lastDistAccuracyMult
 	end
