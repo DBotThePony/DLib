14b82239d25fdfaab5ccf735115261ca8dd5a618
DBotThePony <dbotthepony@yandex.ru>
2020-02-05 17:46:07 +0700

    i am getting older

--- vll/build.sh
+++ vll/build.sh
@@ -2,7 +2,7 @@
 moonc -t . bundle.moon fs.moon init.moon util.moon vm_def.moon vm.moon commands.moon hud.moon
 
 echo "
--- Copyright (C) 2018 DBot
+-- Copyright (C) 2018-2020 DBotThePony
 
 -- Permission is hereby granted, free of charge, to any person obtaining a copy
 -- of this software and associated documentation files (the \"Software\"), to deal
