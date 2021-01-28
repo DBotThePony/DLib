3731a5f08ea332227e6940cdcada52a6dfdccec4
DBotThePony <dbotthepony@yandex.ru>
2021-01-28 11:37:46 +0700

    Unroll bnot

--- lua_src/dlib/util/hash.lua
+++ lua_src/dlib/util/hash.lua
@@ -96,40 +96,40 @@ do
 			local a, b, c, d = self.A, self.B, self.C, self.D
 
 			-- /* Round 1 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, bnot(b))))) + (x[ 0]) + 0xd76aa478), S11)) + (b)) % 4294967296 -- /* 1 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, bnot(a))))) + (x[ 1]) + 0xe8c7b756), S12)) + (a)) % 4294967296 -- /* 2 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, bnot(d))))) + (x[ 2]) + 0x242070db), S13)) + (d)) % 4294967296 -- /* 3 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, bnot(c))))) + (x[ 3]) + 0xc1bdceee), S14)) + (c)) % 4294967296 -- /* 4 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, bnot(b))))) + (x[ 4]) + 0xf57c0faf), S11)) + (b)) % 4294967296 -- /* 5 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, bnot(a))))) + (x[ 5]) + 0x4787c62a), S12)) + (a)) % 4294967296 -- /* 6 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, bnot(d))))) + (x[ 6]) + 0xa8304613), S13)) + (d)) % 4294967296 -- /* 7 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, bnot(c))))) + (x[ 7]) + 0xfd469501), S14)) + (c)) % 4294967296 -- /* 8 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, bnot(b))))) + (x[ 8]) + 0x698098d8), S11)) + (b)) % 4294967296 -- /* 9 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, bnot(a))))) + (x[ 9]) + 0x8b44f7af), S12)) + (a)) % 4294967296 -- /* 10 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, bnot(d))))) + (x[10]) + 0xffff5bb1), S13)) + (d)) % 4294967296 -- /* 11 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, bnot(c))))) + (x[11]) + 0x895cd7be), S14)) + (c)) % 4294967296 -- /* 12 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, bnot(b))))) + (x[12]) + 0x6b901122), S11)) + (b)) % 4294967296 -- /* 13 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, bnot(a))))) + (x[13]) + 0xfd987193), S12)) + (a)) % 4294967296 -- /* 14 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, bnot(d))))) + (x[14]) + 0xa679438e), S13)) + (d)) % 4294967296 -- /* 15 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, bnot(c))))) + (x[15]) + 0x49b40821), S14)) + (c)) % 4294967296 -- /* 16 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, -b - 1)))) + (x[ 0]) + 0xd76aa478), S11)) + (b)) % 4294967296 -- /* 1 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, -a - 1)))) + (x[ 1]) + 0xe8c7b756), S12)) + (a)) % 4294967296 -- /* 2 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, -d - 1)))) + (x[ 2]) + 0x242070db), S13)) + (d)) % 4294967296 -- /* 3 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, -c - 1)))) + (x[ 3]) + 0xc1bdceee), S14)) + (c)) % 4294967296 -- /* 4 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, -b - 1)))) + (x[ 4]) + 0xf57c0faf), S11)) + (b)) % 4294967296 -- /* 5 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, -a - 1)))) + (x[ 5]) + 0x4787c62a), S12)) + (a)) % 4294967296 -- /* 6 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, -d - 1)))) + (x[ 6]) + 0xa8304613), S13)) + (d)) % 4294967296 -- /* 7 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, -c - 1)))) + (x[ 7]) + 0xfd469501), S14)) + (c)) % 4294967296 -- /* 8 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, -b - 1)))) + (x[ 8]) + 0x698098d8), S11)) + (b)) % 4294967296 -- /* 9 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, -a - 1)))) + (x[ 9]) + 0x8b44f7af), S12)) + (a)) % 4294967296 -- /* 10 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, -d - 1)))) + (x[10]) + 0xffff5bb1), S13)) + (d)) % 4294967296 -- /* 11 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, -c - 1)))) + (x[11]) + 0x895cd7be), S14)) + (c)) % 4294967296 -- /* 12 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, c), band(d, -b - 1)))) + (x[12]) + 0x6b901122), S11)) + (b)) % 4294967296 -- /* 13 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, b), band(c, -a - 1)))) + (x[13]) + 0xfd987193), S12)) + (a)) % 4294967296 -- /* 14 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, a), band(b, -d - 1)))) + (x[14]) + 0xa679438e), S13)) + (d)) % 4294967296 -- /* 15 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, d), band(a, -c - 1)))) + (x[15]) + 0x49b40821), S14)) + (c)) % 4294967296 -- /* 16 */
 
 			-- /* Round 2 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, bnot(d))))) + (x[ 1]) + 0xf61e2562), S21)) + (b)) % 4294967296 -- /* 17 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, bnot(c))))) + (x[ 6]) + 0xc040b340), S22)) + (a)) % 4294967296 -- /* 18 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, bnot(b))))) + (x[11]) + 0x265e5a51), S23)) + (d)) % 4294967296 -- /* 19 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, bnot(a))))) + (x[ 0]) + 0xe9b6c7aa), S24)) + (c)) % 4294967296 -- /* 20 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, bnot(d))))) + (x[ 5]) + 0xd62f105d), S21)) + (b)) % 4294967296 -- /* 21 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, bnot(c))))) + (x[10]) +  0x2441453), S22)) + (a)) % 4294967296 -- /* 22 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, bnot(b))))) + (x[15]) + 0xd8a1e681), S23)) + (d)) % 4294967296 -- /* 23 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, bnot(a))))) + (x[ 4]) + 0xe7d3fbc8), S24)) + (c)) % 4294967296 -- /* 24 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, bnot(d))))) + (x[ 9]) + 0x21e1cde6), S21)) + (b)) % 4294967296 -- /* 25 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, bnot(c))))) + (x[14]) + 0xc33707d6), S22)) + (a)) % 4294967296 -- /* 26 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, bnot(b))))) + (x[ 3]) + 0xf4d50d87), S23)) + (d)) % 4294967296 -- /* 27 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, bnot(a))))) + (x[ 8]) + 0x455a14ed), S24)) + (c)) % 4294967296 -- /* 28 */
-			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, bnot(d))))) + (x[13]) + 0xa9e3e905), S21)) + (b)) % 4294967296 -- /* 29 */
-			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, bnot(c))))) + (x[ 2]) + 0xfcefa3f8), S22)) + (a)) % 4294967296 -- /* 30 */
-			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, bnot(b))))) + (x[ 7]) + 0x676f02d9), S23)) + (d)) % 4294967296 -- /* 31 */
-			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, bnot(a))))) + (x[12]) + 0x8d2a4c8a), S24)) + (c)) % 4294967296 -- /* 32 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, -d - 1)))) + (x[ 1]) + 0xf61e2562), S21)) + (b)) % 4294967296 -- /* 17 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, -c - 1)))) + (x[ 6]) + 0xc040b340), S22)) + (a)) % 4294967296 -- /* 18 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, -b - 1)))) + (x[11]) + 0x265e5a51), S23)) + (d)) % 4294967296 -- /* 19 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, -a - 1)))) + (x[ 0]) + 0xe9b6c7aa), S24)) + (c)) % 4294967296 -- /* 20 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, -d - 1)))) + (x[ 5]) + 0xd62f105d), S21)) + (b)) % 4294967296 -- /* 21 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, -c - 1)))) + (x[10]) +  0x2441453), S22)) + (a)) % 4294967296 -- /* 22 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, -b - 1)))) + (x[15]) + 0xd8a1e681), S23)) + (d)) % 4294967296 -- /* 23 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, -a - 1)))) + (x[ 4]) + 0xe7d3fbc8), S24)) + (c)) % 4294967296 -- /* 24 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, -d - 1)))) + (x[ 9]) + 0x21e1cde6), S21)) + (b)) % 4294967296 -- /* 25 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, -c - 1)))) + (x[14]) + 0xc33707d6), S22)) + (a)) % 4294967296 -- /* 26 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, -b - 1)))) + (x[ 3]) + 0xf4d50d87), S23)) + (d)) % 4294967296 -- /* 27 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, -a - 1)))) + (x[ 8]) + 0x455a14ed), S24)) + (c)) % 4294967296 -- /* 28 */
+			a = (overflow(rotate(((a) + overflow((bor(band(b, d), band(c, -d - 1)))) + (x[13]) + 0xa9e3e905), S21)) + (b)) % 4294967296 -- /* 29 */
+			d = (overflow(rotate(((d) + overflow((bor(band(a, c), band(b, -c - 1)))) + (x[ 2]) + 0xfcefa3f8), S22)) + (a)) % 4294967296 -- /* 30 */
+			c = (overflow(rotate(((c) + overflow((bor(band(d, b), band(a, -b - 1)))) + (x[ 7]) + 0x676f02d9), S23)) + (d)) % 4294967296 -- /* 31 */
+			b = (overflow(rotate(((b) + overflow((bor(band(c, a), band(d, -a - 1)))) + (x[12]) + 0x8d2a4c8a), S24)) + (c)) % 4294967296 -- /* 32 */
 
 			-- /* Round 3 */
 			a = (overflow(rotate(((a) + overflow(bxor(b, c, d)) + (x[ 5]) + 0xfffa3942), S31)) + (b)) % 4294967296 -- /* 33 */
@@ -150,22 +150,22 @@ do
 			b = (overflow(rotate(((b) + overflow(bxor(c, d, a)) + (x[ 2]) + 0xc4ac5665), S34)) + (c)) % 4294967296 -- /* 48 */
 
 			-- /* Round 4 */
-			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, bnot(d))))) + (x[ 0]) + 0xf4292244), S41)) + (b)) % 4294967296 -- /* 49 */
-			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, bnot(c))))) + (x[ 7]) + 0x432aff97), S42)) + (a)) % 4294967296 -- /* 50 */
-			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, bnot(b))))) + (x[14]) + 0xab9423a7), S43)) + (d)) % 4294967296 -- /* 51 */
-			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, bnot(a))))) + (x[ 5]) + 0xfc93a039), S44)) + (c)) % 4294967296 -- /* 52 */
-			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, bnot(d))))) + (x[12]) + 0x655b59c3), S41)) + (b)) % 4294967296 -- /* 53 */
-			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, bnot(c))))) + (x[ 3]) + 0x8f0ccc92), S42)) + (a)) % 4294967296 -- /* 54 */
-			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, bnot(b))))) + (x[10]) + 0xffeff47d), S43)) + (d)) % 4294967296 -- /* 55 */
-			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, bnot(a))))) + (x[ 1]) + 0x85845dd1), S44)) + (c)) % 4294967296 -- /* 56 */
-			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, bnot(d))))) + (x[ 8]) + 0x6fa87e4f), S41)) + (b)) % 4294967296 -- /* 57 */
-			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, bnot(c))))) + (x[15]) + 0xfe2ce6e0), S42)) + (a)) % 4294967296 -- /* 58 */
-			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, bnot(b))))) + (x[ 6]) + 0xa3014314), S43)) + (d)) % 4294967296 -- /* 59 */
-			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, bnot(a))))) + (x[13]) + 0x4e0811a1), S44)) + (c)) % 4294967296 -- /* 60 */
-			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, bnot(d))))) + (x[ 4]) + 0xf7537e82), S41)) + (b)) % 4294967296 -- /* 61 */
-			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, bnot(c))))) + (x[11]) + 0xbd3af235), S42)) + (a)) % 4294967296 -- /* 62 */
-			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, bnot(b))))) + (x[ 2]) + 0x2ad7d2bb), S43)) + (d)) % 4294967296 -- /* 63 */
-			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, bnot(a))))) + (x[ 9]) + 0xeb86d391), S44)) + (c)) % 4294967296 -- /* 64 */
+			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, -d - 1)))) + (x[ 0]) + 0xf4292244), S41)) + (b)) % 4294967296 -- /* 49 */
+			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, -c - 1)))) + (x[ 7]) + 0x432aff97), S42)) + (a)) % 4294967296 -- /* 50 */
+			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, -b - 1)))) + (x[14]) + 0xab9423a7), S43)) + (d)) % 4294967296 -- /* 51 */
+			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, -a - 1)))) + (x[ 5]) + 0xfc93a039), S44)) + (c)) % 4294967296 -- /* 52 */
+			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, -d - 1)))) + (x[12]) + 0x655b59c3), S41)) + (b)) % 4294967296 -- /* 53 */
+			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, -c - 1)))) + (x[ 3]) + 0x8f0ccc92), S42)) + (a)) % 4294967296 -- /* 54 */
+			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, -b - 1)))) + (x[10]) + 0xffeff47d), S43)) + (d)) % 4294967296 -- /* 55 */
+			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, -a - 1)))) + (x[ 1]) + 0x85845dd1), S44)) + (c)) % 4294967296 -- /* 56 */
+			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, -d - 1)))) + (x[ 8]) + 0x6fa87e4f), S41)) + (b)) % 4294967296 -- /* 57 */
+			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, -c - 1)))) + (x[15]) + 0xfe2ce6e0), S42)) + (a)) % 4294967296 -- /* 58 */
+			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, -b - 1)))) + (x[ 6]) + 0xa3014314), S43)) + (d)) % 4294967296 -- /* 59 */
+			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, -a - 1)))) + (x[13]) + 0x4e0811a1), S44)) + (c)) % 4294967296 -- /* 60 */
+			a = (overflow(rotate(((a) + overflow((bxor(c, bor(b, -d - 1)))) + (x[ 4]) + 0xf7537e82), S41)) + (b)) % 4294967296 -- /* 61 */
+			d = (overflow(rotate(((d) + overflow((bxor(b, bor(a, -c - 1)))) + (x[11]) + 0xbd3af235), S42)) + (a)) % 4294967296 -- /* 62 */
+			c = (overflow(rotate(((c) + overflow((bxor(a, bor(d, -b - 1)))) + (x[ 2]) + 0x2ad7d2bb), S43)) + (d)) % 4294967296 -- /* 63 */
+			b = (overflow(rotate(((b) + overflow((bxor(d, bor(c, -a - 1)))) + (x[ 9]) + 0xeb86d391), S44)) + (c)) % 4294967296 -- /* 64 */
 
 			self.A = (self.A + overflow(a)) % 4294967296
 			self.B = (self.B + overflow(b)) % 4294967296
